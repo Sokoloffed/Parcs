9b76b6d8df600503477de207c6f092ccd084e96d
unknown <dansru@gmail.com>
2019-11-25 11:29:50 +0200

    Initial commit

---
+++ src/rhopollard.java
@@ -0,0 +1,61 @@
+import parcs.*;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+
+
+public class PollardRho implements AM{
+    private final static BigInteger ZERO = new BigInteger("0");
+    private final static BigInteger ONE  = new BigInteger("1");
+    private final static BigInteger TWO  = new BigInteger("2");
+    private final static SecureRandom random = new SecureRandom();
+
+
+    public static BigInteger rho(BigInteger N) {
+        BigInteger divisor;
+        BigInteger c  = new BigInteger(N.bitLength(), random);
+        BigInteger x  = new BigInteger(N.bitLength(), random);
+        BigInteger xx = x;
+
+        // check divisibility by 2
+        if (N.mod(TWO).compareTo(ZERO) == 1) return TWO;
+
+        do {
+            x  =  x.multiply(x).mod(N).add(c).mod(N);
+            xx = xx.multiply(xx).mod(N).add(c).mod(N);
+            xx = xx.multiply(xx).mod(N).add(c).mod(N);
+            divisor = x.subtract(xx).gcd(N);
+        } while((divisor.compareTo(ONE)) == 0);
+
+        return divisor;
+    }
+
+    public void run(AMInfo info){
+        BigInteger n;
+	ArrayList<BigInteger> r = new ArrayList<BigInteger>();
+        n = BigInteger(info.parent.readObject().toString());
+
+        if (n.isProbablePrime(1) || n.compareTo(ONE) == 0) r.add(n);
+	else
+	{ 
+	    BigInteger divisor = rho(n);
+
+            point p1 = info.createPoint();
+            channel c1 = p1.createChannel();
+            p1.execute("PollardRho");
+            c1.write(divisor.toString());
+
+            point p2 = info.createPoint();
+            channel c2 = p2.createChannel();
+            p2.execute("PollardRho");
+            c2.write(n.divide(divisor).toString());
+
+            ArrayList<BigInteger> r1 = ArrayList<BigInteger>(c1.readObject());
+ 	    ArrayList<BigInteger> r1 = ArrayList<BigInteger>(c1.readObject());
+
+            r.addAll(r1);
+	    r.addAll(r2);
+        }
+        info.parent.write(r);
+    }
+}
---
+++ src/rhopollardp.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import parcs.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class RhoPollardP implements AM {
+
+    public static void main(String[] args) {
+        task curtask = new task();
+        curtask.addJarFile("RhoPollardP.jar");
+        (new RhoPollardP()).run(new AMInfo(curtask, (channel)null));
+        curtask.end();
+    }
+
+    public void run(AMInfo info) {
+      BigInteger n;
+      try {
+          System.out.print("Enter n: ");
+          canner sc = new Scanner(System.in);
+	  n = sc.nextBigInteger();
+
+      } catch (IOException e) {e.printStackTrace(); return;}
+
+      point p1 = info.createPoint();
+      channel c1 = p1.createChannel();
+      p1.execute("RhoPollard");
+      c1.write(n.toString());
+
+      System.out.println("Waiting for result...");
+      ArrayList<BigInteger> r = ArrayList<BigInteger>(c1.readLong());
+      System.out.println("Result found.");
+
+      System.out.println(Arrays.toString(r));
+    }
+}
