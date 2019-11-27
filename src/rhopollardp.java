267d15adaa748a1fab7d63c88a076a6cab3df5e5
unknown <dansru@gmail.com>
2019-11-27 12:35:32 +0200

    Fixxxx

--- src/rhopollardp.java
+++ src/rhopollardp.java
@@ -13,8 +13,6 @@ public class RhoPollardP implements AM {
         curtask.addJarFile("RhoPollardP.jar");
         (new RhoPollardP()).run(new AMInfo(curtask, (channel)null));
         curtask.end();
-        // TODO: Modified name
-        // TODO: More attempts
 
     }
 
